e37ffc8e12e5e64710b8adbc57b43c0938a12c41
Chris <c.urffer@live.com>
2025-01-15 00:39:12 -0500

    switched to 'macroquad' graphics library (much simpler)

--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,23 @@
-#[cfg(not(target_arch = "wasm32"))]
-use bevy::sprite::{Wireframe2dConfig, Wireframe2dPlugin};
-use bevy::{
-    prelude::*,
-    sprite::{MaterialMesh2dBundle, Mesh2dHandle},
-};
+use macroquad::prelude::*;
 
-use crate::rustree::*;
+use crate::rustree::Rustree;
 
 pub mod rustree;
 
-fn main() {
-    let mut app = App::new();
-    app.add_plugins((
-        DefaultPlugins,
-        #[cfg(not(target_arch = "wasm32"))]
-        Wireframe2dPlugin,
-    ))
-    .add_systems(Startup, setup);
-    #[cfg(not(target_arch = "wasm32"))]
-    app.add_systems(Update, toggle_wireframe);
-    app.run();
-}
+#[macroquad::main("rustrees")]
+async fn main() {
 
-const X_EXTENT: f32 = 900.;
+    let tree = Rustree::new(100, 200, 30);
 
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
-    commands.spawn(Camera2dBundle::default());
+    loop {
+        clear_background(BLUE);
 
-    let shapes = [
-        Mesh2dHandle(meshes.add(Circle { radius: 50.0 })),
-        Mesh2dHandle(meshes.add(CircularSector::new(50.0, 1.0))),
-        Mesh2dHandle(meshes.add(CircularSegment::new(50.0, 1.25))),
-        Mesh2dHandle(meshes.add(Ellipse::new(25.0, 50.0))),
-        Mesh2dHandle(meshes.add(Annulus::new(25.0, 50.0))),
-        Mesh2dHandle(meshes.add(Capsule2d::new(25.0, 50.0))),
-        Mesh2dHandle(meshes.add(Rhombus::new(75.0, 100.0))),
-        Mesh2dHandle(meshes.add(Rectangle::new(50.0, 100.0))),
-        Mesh2dHandle(meshes.add(RegularPolygon::new(50.0, 6))),
-        Mesh2dHandle(meshes.add(Triangle2d::new(
-            Vec2::Y * 50.0,
-            Vec2::new(-50.0, -50.0),
-            Vec2::new(50.0, -50.0),
-        ))),
-    ];
-    let num_shapes = shapes.len();
+        tree.draw();
 
-    for (i, shape) in shapes.into_iter().enumerate() {
-        // Distribute colors evenly across the rainbow.
-        let color = Color::hsl(360. * i as f32 / num_shapes as f32, 0.95, 0.7);
+        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
 
-        commands.spawn(MaterialMesh2dBundle {
-            mesh: shape,
-            material: materials.add(color),
-            transform: Transform::from_xyz(
-                // Distribute shapes from -X_EXTENT/2 to +X_EXTENT/2.
-                -X_EXTENT / 2. + i as f32 / (num_shapes - 1) as f32 * X_EXTENT,
-                0.0,
-                0.0,
-            ),
-            ..default()
-        });
-    }
+        draw_text("Hello, Macroquad!", 20.0, 20.0, 30.0, DARKGRAY);
 
-    #[cfg(not(target_arch = "wasm32"))]
-    commands.spawn(
-        TextBundle::from_section("Press space to toggle wireframes", TextStyle::default())
-            .with_style(Style {
-                position_type: PositionType::Absolute,
-                top: Val::Px(12.0),
-                left: Val::Px(12.0),
-                ..default()
-            }),
-    );
-}
-
-#[cfg(not(target_arch = "wasm32"))]
-fn toggle_wireframe(
-    mut wireframe_config: ResMut<Wireframe2dConfig>,
-    keyboard: Res<ButtonInput<KeyCode>>,
-) {
-    if keyboard.just_pressed(KeyCode::Space) {
-        wireframe_config.global = !wireframe_config.global;
+        next_frame().await
     }
 }
--- src/rustree.rs
+++ src/rustree.rs
@@ -1,3 +1,5 @@
+use macroquad::prelude::*;
+
 enum TreeNodeType {
 	Struct,
 	Leaf,
@@ -16,6 +18,39 @@ struct TreeNode {
     activated: bool,
 }
 
-struct Rustree {
-    root: (i32, i32),
+pub struct Rustree {
+    nodes: Vec<TreeNode>,
+}
+
+impl Rustree {
+    pub fn new(x: i32, y: i32, size: i32) -> Rustree {
+        let root_node: TreeNode = TreeNode {
+            node_type: TreeNodeType::Struct,
+            size,
+            dist: 0.0,
+            angle: 0,
+            pos: (x, y),
+            activated: false,
+        };
+
+        let mut node_list = Vec::new();
+        node_list.push(root_node);
+
+        Rustree {
+            nodes: node_list,
+        }
+    }
+
+    fn root(&self) -> &TreeNode {
+        &self.nodes[0]
+    }
+
+    pub fn draw(&self) -> () {
+        draw_circle(
+            self.root().pos.0 as f32,
+            self.root().pos.1 as f32,
+            self.root().size as f32,
+        BROWN
+        );
+    }
 }
