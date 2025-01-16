1bf919abb972a1e932cc80f9a729c38b9324bcdf
Chris <c.urffer@live.com>
2025-01-16 00:02:09 -0500

    WIP (broken): learning about lifetimes and generics

--- src/rustree.rs
+++ src/rustree.rs
@@ -16,13 +16,70 @@ struct TreeNode {
     pos: (i32, i32),
 
     activated: bool,
+
+    // owner: Option<&Rustree>,
+    // parent: Option<&TreeNode>,
+    children: Vec<TreeNode>,
 }
 
+impl TreeNode {
+    fn new(
+        node_type: TreeNodeType,
+        size: i32,
+        dist: f32,
+        angle: i32,
+        pos: (i32, i32),
+        activated: bool,
+    ) -> TreeNode {
+        TreeNode {
+            node_type,
+            size,
+            dist,
+            angle,
+            pos,
+            activated,
+            children: Vec::new(),
+        }
+    }
+
+    fn add_child(&mut self, child: TreeNode) {
+        self.children.push(child);
+    }
+}
+
+
+
 pub struct Rustree {
-    nodes: Vec<TreeNode>,
+    root: TreeNode,
 }
 
 impl Rustree {
+    pub fn new(
+        x: i32,
+        y: i32,
+    ) -> Rustree {
+        let root_node = TreeNode::new(
+            TreeNodeType::Root,
+            30,
+            0.0,
+            0,
+            (x, y),
+            false,
+        );
+
+        Rustree {
+            root: root_node,
+        }
+    }
+
+    pub fn new_from_data(
+        n: TreeNode,
+    ) -> Rustree {
+        Rustree {
+            root: n,
+        }
+    }
+
     pub fn new(x: i32, y: i32, size: i32) -> Rustree {
         let root_node: TreeNode = TreeNode {
             node_type: TreeNodeType::Struct,
@@ -46,11 +103,29 @@ impl Rustree {
     }
 
     pub fn draw(&self) -> () {
-        draw_circle(
-            self.root().pos.0 as f32,
-            self.root().pos.1 as f32,
-            self.root().size as f32,
-        BROWN
-        );
+        for node in &self.nodes {
+            let color: macroquad::prelude::Color;
+            match node.node_type {
+                TreeNodeType::Struct => {
+                    color = BROWN;
+                }
+                _ => {
+                    color = BLACK;
+                }
+            }
+
+            draw_circle(
+                node.pos.0 as f32,
+                node.pos.1 as f32,
+                node.size as f32,
+                color,
+            );
+        }
+        // draw_circle(
+        //     self.root().pos.0 as f32,
+        //     self.root().pos.1 as f32,
+        //     self.root().size as f32,
+        // BROWN
+        // );
     }
 }
